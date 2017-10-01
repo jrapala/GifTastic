881844d85230b8629ef859b5f0e4051d4a7f7e2e
Juliette Rapala <jrapala@gmail.com>
2017-10-01 18:57:09 -0500

    Adding HTML structure.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,21 @@
 // GifTastic | By Juliette Rapala
 // =====================================================================================
 
-
 $(document).ready(function(){
+
+var celebrities = [
+	"Michael Jackson",
+	"Samuel L. Jackson",
+	"Snoop Dogg",
+	"Kim Kardashian",
+	"Kanye West",
+	"Marilyn Manson",
+	"Taylor Swift",
+	"Beyonce",
+	"Jay-Z",
+	"Kate Middleton"
+]
+
+
 
 });
