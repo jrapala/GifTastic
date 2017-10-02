4bef7c1c133b3be8c2077493f2b8b35167d6047a
Juliette Rapala <jrapala@gmail.com>
2017-10-02 13:50:33 -0500

    Adding functionality to celebrity buttons. When a button is pressed, 10 gifs a celebrity come up.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,19 +3,79 @@
 
 $(document).ready(function(){
 
-var celebrities = [
-	"Michael Jackson",
-	"Samuel L. Jackson",
-	"Snoop Dogg",
-	"Kim Kardashian",
-	"Kanye West",
-	"Marilyn Manson",
-	"Taylor Swift",
-	"Beyonce",
-	"Jay-Z",
-	"Kate Middleton"
-]
+	// Variables 
+	// =====================================================================================
 
+	var celebrities = [
+		"Michael Jackson",
+		"Samuel L. Jackson",
+		"Snoop Dogg",
+		"Kim Kardashian",
+		"Aubrey Plaza",
+		"Marilyn Manson",
+		"Taylor Swift",
+		"Beyonce",
+		"Jay-Z",
+		"Jonah Hill",
+		"Dwayne Johnson",
+		"Drake",
+		"Jennifer Lawrence",
+		"Chris Pratt",
+		"Adele",
+		"Kanye West",
+		"Justin Bieber",
+		"Kit Harrington",
+		"Ellen DeGeneres",
+		"Chance the Rapper",
+		"Amy Poehler"
+	];
 
+	// Functions 
+	// =====================================================================================
 
+	// Create buttons on top of screen using array of celebrities 
+	function init() {
+		for (var i = 0; i < celebrities.length; i++) {
+			var $celebButton = $('<button>');
+			$celebButton.attr("data-celeb", celebrities[i]);
+			$celebButton.addClass("celebButton");
+			$celebButton.text(celebrities[i]);
+			$('#celebrityButtons').append($celebButton);
+		}
+
+		// When celebrity button is clicked, add 10 images using Giphy API
+		$(".celebButton").on("click", function() {
+	    	var celeb = $(this).attr("data-celeb");
+	    	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + celeb + "&api_key=Su8yLiz6okgSl5WLjpwgFOMYO6U1Zf39&limit=10";
+
+	      	$.ajax({
+	        	url: queryURL,
+	          	method: "GET"
+	        })
+	        
+	        .done(function(response) {
+	        	var results = response.data;
+
+	        	for (var i = 0; i < results.length; i++) {
+	        		var gifDiv = $("<div>");
+	        		gifDiv.addClass('image');
+	        		var rating = results[i].rating;
+	        		var ratingParagraph = $("<p>")
+	        		ratingParagraph.text("Rating: " + rating);
+	            	var celebImage = $("<img>");
+	            	celebImage.attr("src", results[i].images.fixed_height.url);
+
+	            	gifDiv.prepend(ratingParagraph);
+	            	gifDiv.prepend(celebImage);
+
+	            	$("#celebrities").prepend(gifDiv);
+	          	}
+	        });
+	    });
+	}
+
+	// Function Calls
+	// =====================================================================================
+
+	init();
 });
