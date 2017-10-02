fed42451b83588489db6090d50a0b5538d9d9808
Juliette Rapala <jrapala@gmail.com>
2017-10-02 14:06:10 -0500

    Adding still and animated image attributes. Changing all gifs to still images upon click of a celeb button.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
 		// When celebrity button is clicked, add 10 images using Giphy API
 		$(".celebButton").on("click", function() {
 	    	var celeb = $(this).attr("data-celeb");
-	    	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + celeb + "&api_key=Su8yLiz6okgSl5WLjpwgFOMYO6U1Zf39&limit=10";
+	    	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + celeb + "&api_key=Su8yLiz6okgSl5WLjpwgFOMYO6U1Zf39&limit=10";
 
 	      	$.ajax({
 	        	url: queryURL,
@@ -58,18 +58,23 @@ $(document).ready(function(){
 
 	        	for (var i = 0; i < results.length; i++) {
 	        		var gifDiv = $("<div>");
-	        		gifDiv.addClass('image');
+	        		gifDiv.addClass('celebImage');
 	        		var rating = results[i].rating;
 	        		var ratingParagraph = $("<p>")
 	        		ratingParagraph.text("Rating: " + rating);
 	            	var celebImage = $("<img>");
-	            	celebImage.attr("src", results[i].images.fixed_height.url);
-
+	            	var stillImage = results[i].images.fixed_height_still.url
+	            	var animatedImage = results[i].images.fixed_height.url
+	            	celebImage.attr("data-still", stillImage);
+	            	celebImage.attr("data-animated", animatedImage);
+	            	celebImage.attr("data-state", "still");
+	            	celebImage.attr("src", stillImage);
 	            	gifDiv.prepend(ratingParagraph);
 	            	gifDiv.prepend(celebImage);
-
 	            	$("#celebrities").prepend(gifDiv);
+	            	//console.log(response);
 	          	}
+
 	        });
 	    });
 	}
