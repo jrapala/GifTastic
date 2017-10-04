4d7846f8011a4dafc48b4665b8615933c4627e2c
Juliette Rapala <jrapala@gmail.com>
2017-10-04 13:26:14 -0500

    Code clean up. Adding comments.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -33,7 +33,9 @@
 
 	// Create buttons on top of screen using array of celebrities 
 	function createRowOfButtons() {
+		// Empty current div of buttons
 		$('#celebrityButtons').empty();
+		// Loop through list of celebrites and create buttons for each listing
 		for (var i = 0; i < celebrities.length; i++) {
 			var $celebButton = $('<button>');
 			$celebButton.attr("data-celeb", celebrities[i]);
@@ -46,24 +48,29 @@
 
 	$(document).ready(function(){
 
+		// Create celebrity buttons on top of screen
 		createRowOfButtons();
 
-		// Add celebrity button
+		// Allow user to add a celebrity button using the submit button
 		$("#addCelebrity").on("click", function(e) {
+			// Collect user input
 			var userCeleb = $("#celebrity-input").val().trim();
-			// Check if user entered input is in array
+			// Check if user entered input is already in array. Alert user if found.
 			if (celebrities.indexOf(userCeleb) >= 0) {
 				alert("That celebrity button already exists!\nLet's get another celebrity in here!");
-			// Check if user entered input to prevent empty buttons
+			// Check if user entered some input to prevent creation of empty buttons
 			} else if (userCeleb.length > 0) {
 				celebrities.push(userCeleb);
+				// Refresh buttons on top of page
 				createRowOfButtons();
 			} 
 		});
 
 		// When celebrity button is clicked, add 10 images using Giphy API
 		$("#celebrityButtons").delegate(".celebButton", "click", function() {
+			// Collect the name of the celebrity stored in the data attribute
 	    	var celeb = $(this).attr("data-celeb");
+	    	// API query and request, limited to 10 gifs
 	    	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + celeb + "&api_key=Su8yLiz6okgSl5WLjpwgFOMYO6U1Zf39&limit=10";
 
 	      	$.ajax({
@@ -73,6 +80,7 @@
 	        
 	        .done(function(response) {
 	        	var results = response.data;
+	        	// Create 10 divs, each containing one celebrity gif still and gif rating
 	        	for (var i = 0; i < results.length; i++) {
 	        		var gifDiv = $("<div>");
 	        		gifDiv.addClass('gifDiv')
@@ -92,8 +100,11 @@
 	            	$("#celebrities").prepend(gifDiv);
 	          	}
 	          	
+	          	// When a gif still is clicked, animate the gif. When an animated gif if clicked, display a still.
 	        	$(".celebImage").on('click', function () {
+	        		// Collect the current state of the gif stored in the data attribute
 	        		var state = $(this).attr('data-state');  
+
 	    			if (state === "still") {
 	      				$(this).attr('data-state', 'animate');
 	      				$(this).attr('src', $(this).attr('data-animate'));  
@@ -107,6 +118,3 @@
 	        });
 	    });
 	});
-
-
-// $("#mainbuttons").delegate(".buttons", "click" function)
